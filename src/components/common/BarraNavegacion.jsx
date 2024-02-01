c3b05d1971507f3a3abc1f8ef16abf55ee869a9c
Lucas Capdevila <lcapdevila60@gmail.com>
2024-02-01 17:53:36 -0300

    se agrega componentes Navbar, Footer e Inicio

---
+++ src/components/common/BarraNavegacion.jsx
@@ -0,0 +1,9 @@
+const BarraNavegacion = () => {
+  return (
+    <div>
+      <p>Aquí va el Navbar</p>
+    </div>
+  )
+}
+
+export default BarraNavegacion
---
+++ src/components/pages/Inicio.jsx
@@ -0,0 +1,9 @@
+const Inicio = () => {
+  return (
+    <div>
+      <p>Aquí irá el 'main' de la página principal</p>
+    </div>
+  )
+}
+
+export default Inicio
