10ce5330f9c6caba880e3306b22a652a1a61b168
Lucas Capdevila <lcapdevila60@gmail.com>
2024-02-03 16:50:43 -0300

    se agrega título y se baja el footer

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Button, Container } from 'react-bootstrap'
 
 const Administrador = () => {
   return (
-    <main>
+    <main className='mainPage'>
       <Container>
-        
+        <div className='d-flex align-items-center justify-content-between'>
+          <h2 className="display-3">Productos disponibles</h2>
+          <Button className=''>asd</Button>
+        </div>
       </Container>
     </main>
   )
