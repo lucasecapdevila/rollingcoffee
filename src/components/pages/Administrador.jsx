59eb88f4494a7c469b5b07ac286d4ba7d9059b9f
Lucas Capdevila <lcapdevila60@gmail.com>
2024-02-05 17:45:02 -0300

    se agrega tabla al maquetado de página de administrador

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
-import { Button, Container } from 'react-bootstrap'
+import { faPenToSquare, faPlus, faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import { Button, Container, Table } from 'react-bootstrap'
+import producto from '../../assets/coffee-producto.jpg'
 
 const Administrador = () => {
   return (
@@ -10,6 +11,74 @@ const Administrador = () => {
           <h2 className="display-3">Productos disponibles</h2>
           <Button className=''><FontAwesomeIcon icon={faPlus} /></Button>
         </div>
+        <hr />
+
+        <Table striped bordered responsive>
+          <thead>
+            <tr>
+              <th>Cod</th>
+              <th>Producto</th>
+              <th>Precio</th>
+              <th>URL de Imagen</th>
+              <th>Categoría</th>
+              <th>Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Café americano</td>
+              <td>$250</td>
+              <td className='w-25'><img src={producto} className='img-fluid' /></td>
+              <td>Bebida caliente</td>
+              <td>
+                <div className='d-flex align-items-center justify-content-center'>
+                  <Button variant='warning' className='me-2'><FontAwesomeIcon icon={faPenToSquare} /></Button>
+                  <Button variant='danger'><FontAwesomeIcon icon={faTrashCan} /></Button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Capuchino</td>
+              <td>$350</td>
+              <td className='w-25'><img src={producto} className='img-fluid' /></td>
+              <td>Bebida caliente</td>
+              <td>
+                <div className='d-flex align-items-center justify-content-center'>
+                  <Button variant='warning' className='me-2'><FontAwesomeIcon icon={faPenToSquare} /></Button>
+                  <Button variant='danger'><FontAwesomeIcon icon={faTrashCan} /></Button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Croissant de chocolate</td>
+              <td className='me-0'>$180</td>
+              <td className='w-25'><img src={producto} className='img-fluid' /></td>
+              <td>Repostería</td>
+              <td>
+                <div className='d-flex align-items-center justify-content-center'>
+                  <Button variant='warning' className='me-2'><FontAwesomeIcon icon={faPenToSquare} /></Button>
+                  <Button variant='danger'><FontAwesomeIcon icon={faTrashCan} /></Button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>Té verde</td>
+              <td>$200</td>
+              <td className='w-25'><img src={producto} className='img-fluid' /></td>
+              <td>Bebida caliente</td>
+              <td>
+                <div className='d-flex align-items-center justify-content-center'>
+                  <Button variant='warning' className='me-2'><FontAwesomeIcon icon={faPenToSquare} /></Button>
+                  <Button variant='danger'><FontAwesomeIcon icon={faTrashCan} /></Button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
       </Container>
     </main>
   )
