6bf635b4f61d06dd9d7b0268be5495068bf24407
Lucas Capdevila <lcapdevila60@gmail.com>
2024-02-08 18:34:12 -0300

    se agregan validaciones para crear un producto

--- src/components/pages/producto/FormularioProducto.jsx
+++ src/components/pages/producto/FormularioProducto.jsx
@@ -1,31 +1,52 @@
 import { Form, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form"
+
 
 const FormularioProducto = () => {
+  const {register, handleSubmit, formState: { errors }} = useForm()
+
+  const productoValidado = (producto) => {
+    console.log(producto);
+  }
 
   return (
     <section className="container mainSection">
       <h1 className="display-4 mt-5">Nuevo producto</h1>
       <hr />
-      {/* <Form onSubmit={handleSubmit}> */}
-      <Form className="my-4">
+      <Form onSubmit={handleSubmit(productoValidado)} className="my-4" >
         <Form.Group className="mb-3" controlId="formNombreProdcuto">
           <Form.Label>Producto*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ej: Cafe"
+            {
+              ...register("nombreProducto",{
+                required: 'El nombre del producto es obligatorio.',
+                minLength:{
+                  value: 2,
+                  message: 'Debe ingresar como mínimo 2 carácteres para el nombre del producto.'
+                },
+                maxLength:{
+                  value: 50,
+                  message: 'Debe ingresar como máximo 50 carácteres para el nombre del producto.'
+                }
+              })
+            }
           />
-          <Form.Text className="text-danger">
-            prueba de error
-          </Form.Text>
+          <Form.Text className="text-danger">{errors.nombreProducto?.message}</Form.Text>
+          
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Precio*</Form.Label>
           <Form.Control
             type="number"
             placeholder="Ej: 50"
+            // {
+            //   ...register("precio")
+            // }
           />
           <Form.Text className="text-danger">
-          prueba de error
+            prueba de error
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
@@ -33,6 +54,9 @@ const FormularioProducto = () => {
           <Form.Control
             type="text"
             placeholder="Ej: https://www.pexels.com/es-es/vans-en-blanco-y-negro-fuera-de-la-decoracion-para-colgar-en-la-pared-1230679/"
+            // {
+            //   ...register("imagen")
+            // }
           />
           <Form.Text className="text-danger">
         prueba de error
@@ -40,7 +64,7 @@ const FormularioProducto = () => {
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Categoría*</Form.Label>
-          <Form.Select>
+          <Form.Select required>
             <option value="">Seleccione una opcion</option>
             <option value="Infusiones">Infusiones</option>
             <option value="Batidos">Batidos</option>
