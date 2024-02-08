d3da7868003cd82acd640a3dfac330b672d4910b
Lucas Capdevila <lcapdevila60@gmail.com>
2024-02-08 17:16:10 -0300

    agregamos la función para leer los productos

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Administrador from './components/pages/Administrador'
 import Error404 from './components/pages/Error404'
 import Inicio from './components/pages/Inicio'
 import DetalleProducto from './components/pages/producto/DetalleProducto'
+import FormularioProducto from './components/pages/producto/FormularioProducto'
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Inicio></Inicio>} />
         <Route exact path='/admin' element={<Administrador></Administrador>} />
-        <Route exact path='/admin/crear' element={<Administrador></Administrador>} />
+        <Route exact path='/admin/crear' element={<FormularioProducto></FormularioProducto>} />
         <Route exact path='/admin/editar' element={<Administrador></Administrador>} />
         <Route exact path='/detalle' element={<DetalleProducto></DetalleProducto>} />
         <Route path='*' element={<Error404></Error404>} />
