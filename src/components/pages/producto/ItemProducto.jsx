4e2b310b05938d9cdfc4a83fab45924f9d594f1c
Lucas Capdevila <lcapdevila60@gmail.com>
2024-02-15 18:29:28 -0300

    agregamos la funcion borrarProducto en el componente ItemProducto

--- src/components/pages/producto/ItemProducto.jsx
+++ src/components/pages/producto/ItemProducto.jsx
@@ -1,9 +1,34 @@
 import { Button } from 'react-bootstrap'
 import { faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Swal from 'sweetalert2'
+import { borrarProductosAPI } from '../../../helpers/queries'
 
 
 const ItemProducto = ({producto}) => {
+  const borrarProducto = () => {
+    Swal.fire({
+      title: "¿Estás seguro de eliminar el producto?",
+      text: "No podrás revertir este proceso.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Borrar",
+      cancelButtonText: "Cancelar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        borrarProductosAPI(producto.id)
+        
+        Swal.fire({
+          title: "Eliminado!",
+          text: "El producto se eliminó exitosamente.",
+          icon: "success"
+        });
+      }
+    });
+  }
+
   return (
     <tr>
       <td>{producto.id}</td>
@@ -14,7 +39,7 @@ const ItemProducto = ({producto}) => {
       <td>
         <div className='d-flex align-items-center justify-content-center'>
           <Button variant='warning' className='me-2'><FontAwesomeIcon icon={faPenToSquare} /></Button>
-          <Button variant='danger'><FontAwesomeIcon icon={faTrashCan} /></Button>
+          <Button variant='danger' onClick={borrarProducto}><FontAwesomeIcon icon={faTrashCan} /></Button>
         </div>
       </td>
     </tr>
--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -2,7 +2,7 @@ const URI_PRODUCTOS = import.meta.env.VITE_API_PRODUCTOS
 
 console.log(URI_PRODUCTOS);
 
-//  Solicitud tipo GET (o Request)
+//  Solicitud tipo GET (o Request) - Editar
 export const leerProductosAPI = async() => {
   try {
     const respuesta = await fetch(URI_PRODUCTOS)
@@ -16,7 +16,7 @@ export const leerProductosAPI = async() => {
   }
 }
 
-//  Solicitud tipo POST
+//  Solicitud tipo POST (crear)
 export const crearProductosAPI = async(productoNuevo) => {
   try {
     const respuesta = await fetch(URI_PRODUCTOS, {
@@ -25,6 +25,20 @@ export const crearProductosAPI = async(productoNuevo) => {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(productoNuevo)
+    })
+    console.log(respuesta);
+    return respuesta
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//  Solicitud tipo DELETE (eliminar)
+export const borrarProductosAPI = async(id) => {
+  try {
+    const respuesta = await fetch(`${URI_PRODUCTOS}/${id}`, {
+      method: "DELETE",
     })
     console.log(respuesta);
     return respuesta
