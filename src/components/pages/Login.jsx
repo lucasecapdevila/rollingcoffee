565fb41c9084b5ef9f5f42e5f8d26614c3a7af08
Lucas Capdevila <lcapdevila60@gmail.com>
2024-02-18 20:11:32 -0300

    se crea la lógica para el formulario de ingreso

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Card, CardBody, CardHeader, Container, Form } from 'react-bootstrap'
 
 const Login = () => {
+  const [form, setForm] = useState({
+    correo: "",
+    password: ""
+  })
+
+  //  Funciones
+  const onUpdateField = (e) => {
+    const nuevoFormState = {
+      ...form,
+      [e.target.name]: e.target.value
+    }
+    setForm(nuevoFormState)
+  }
+
+  const onSubmitForm = (e) => {
+    e.preventDefault()
+    console.log('Funcionando');
+    alert(JSON.stringify(form, null, 2))
+  }
+
   return (
     <main className='mainPage'>
       <Container>
@@ -10,16 +30,28 @@ const Login = () => {
             <h1 className='h3 m-0'>Login</h1>
           </CardHeader>
           <CardBody>
-            <Form>
+            <Form onSubmit={onSubmitForm}>
               <Form.Group className="mb-3" controlId="correo">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="ejemplo@mail.com" />
+                <Form.Control 
+                  type="email"
+                  name="correo"
+                  placeholder="ejemplo@mail.com"
+                  value={form.email}
+                  onChange={onUpdateField}
+                />
               </Form.Group>
               <Form.Group className="mb-3" controlId="password">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Contraseña" />
+                <Form.Control
+                  type="password"
+                  name='password'
+                  placeholder="Contraseña"
+                  value={form.password}
+                  onChange={onUpdateField}
+                />
               </Form.Group>
-              <Button>Ingresar</Button>
+              <Button type='submit'>Ingresar</Button>
             </Form>
           </CardBody>
         </Card>
