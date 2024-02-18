0eda11486950c0a9cae3e20624a59a41496b39a7
Lucas Capdevila <lcapdevila60@gmail.com>
2024-02-18 19:17:31 -0300

    se finaliza maquetado de página de Login

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Button, Card, CardBody, CardHeader, Container, Form } from 'react-bootstrap'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <main className='mainPage'>
+      <Container>
+        <Card>
+          <CardHeader>
+            <h1 className='h3 m-0'>Login</h1>
+          </CardHeader>
+          <CardBody>
+            <Form>
+              <Form.Group className="mb-3" controlId="correo">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="ejemplo@mail.com" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Contraseña" />
+              </Form.Group>
+              <Button>Ingresar</Button>
+            </Form>
+          </CardBody>
+        </Card>
+      </Container>
+    </main>
   )
 }
 
